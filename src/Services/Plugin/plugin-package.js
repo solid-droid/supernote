e809c424679963c063ed8763aed231879ba9a56b
Nikhil M Jeby <nikhilmjeby.mec@gmail.com>
2026-05-24 18:42:40 +0530

    plugin-service changes

--- src/Services/Plugin/plugin-package.js
+++ src/Services/Plugin/plugin-package.js
@@ -1,13 +1,8 @@
 export const pluginPackage = {
-  generatedAt: '2026-05-20T00:00:00.000Z',
   mode: 'default',
   plugins: [
-    { slug: 'button', version: '1.0.1', variants: ['primary', 'secondary', 'tertiary'] },
-    { slug: 'input', version: '1.0.0', variants: ['outlined', 'filled'] },
-    { slug: 'card', version: '1.0.0' },
-    { slug: 'theme', version: '1.0.0', variants: ['dark', 'modern'] },
-    { slug: 'popup', version: '1.0.0' },
-    { slug: 'form', version: '1.0.0' }
+    { slug: 'button', version: '1.0.0', variants: ['flat', 'bordered', 'icon', 'icon-text'] },
+    { slug: 'input', version: '1.0.0', variants: ['text', 'search', 'checkbox'] },
   ]
 };
 
